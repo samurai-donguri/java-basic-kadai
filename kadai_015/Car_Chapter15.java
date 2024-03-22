4f16c14df1183604bbafa404b802db07bbf80b92
samurai-donguri <161983460+samurai-donguri@users.noreply.github.com>
2024-03-22 15:40:08 -0700

    Add files via upload

---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,32 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+	
+    private int gear  = 1;
+    private int speed = 10;
+    
+    public Car_Chapter15( int gear, int speed ) {
+        this.gear  = gear;
+        this.speed = speed;
+    }
+       
+    public void gearChange( final int afterGear ) {
+    	System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました" );
+    	
+    	this.gear = afterGear;
+    	
+    	this.speed = switch(afterGear) {
+    		case 1  -> 10;
+    		case 2  -> 20;
+    		case 3  -> 30;
+    		case 4  -> 40;
+    		case 5  -> 50;
+    		default -> 10;
+        };
+    	
+    }
+    
+    public void run() {
+    	System.out.println("速度は時速" + this.speed + "kmです" );
+    }
+}
